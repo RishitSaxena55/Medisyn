9a477e5a72529c72d89738b680c94d1e2de9b306
Rishit Saxena <rishitsaxena55@gmail.com>
2025-08-15 13:58:11 +0530

    partial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from fastapi import FastAPI
+from graph import graph
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class ChatInput(BaseModel):
+    messages: list[str]
+    thread_id: str
+
+
+@app.post("/chat")
+async def chat(input: ChatInput):
+    config = {"configurable": {"thread_id": input.thread_id}}
+    response = await graph.ainvoke({"messages": input.messages}, config=config)
+    return response["messages"][-1].content
